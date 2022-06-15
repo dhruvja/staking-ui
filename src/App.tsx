5cc5507591916d072e3296e29425c8d670df8a4c
Andrés Mechali <andresmechali@gmail.com>
2022-06-15 13:28:15 +0200

    Implement base app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useMemo, useState } from "react";
+import { Container, TextField, Divider, Alert } from "@mui/material";
+import "./App.css";
+
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { clusterApiUrl } from "@solana/web3.js";
+import {
+  GlowWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+
+import Navbar from "./Navbar";
+import StakeButton from "./StakeButton";
+
+require("@solana/wallet-adapter-react-ui/styles.css");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [amount, setAmount] = useState<number>(0);
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [isOk, setIsOk] = useState<boolean | undefined>(undefined);
+
+  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+  const network = WalletAdapterNetwork.Devnet;
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new GlowWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+    ],
+    [network]
+  );
+
+  const confirmSuccess = useCallback(() => {
+    setIsOk(true);
+  }, []);
+
+  const renderAlert = () => {
+    if (isOk) {
+      return (
+        <Alert
+          sx={{ mt: "16px" }}
+          severity="success"
+          onClose={() => {
+            setIsOk(false);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Successful transaction
+        </Alert>
+      );
+    }
+    if (error) {
+      return (
+        <Alert
+          sx={{ mt: "16px" }}
+          severity="error"
+          onClose={() => {
+            setError(undefined);
+          }}
+        >
+          {error}
+        </Alert>
+      );
+    }
+  };
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Navbar />
+
+          <div className="staking__wrapper">
+            <Container className="staking__container" fixed>
+              <TextField
+                id="staking-amount"
+                label="Amount"
+                type="search"
+                value={amount}
+                onChange={(e) => {
+                  if (!isNaN(Number(e.target.value))) {
+                    setAmount(Number(e.target.value));
+                  }
+                }}
+              />
+              <Divider orientation="vertical" variant="middle" flexItem />
+              <StakeButton
+                amount={amount}
+                setError={setError}
+                confirmSuccess={confirmSuccess}
+              />
+              {renderAlert()}
+            </Container>
+          </div>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+declare module "@mui/material/styles" {
+  interface Theme {
+    status: {
+      danger: string;
+    };
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    };
+  }
+}
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#264653",
+    },
+    warning: {
+      main: "#e9c46a",
+    },
+    success: {
+      main: "#2a9d8f",
+    },
+  },
+  status: {
+    danger: "#e76f51",
+  },
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
