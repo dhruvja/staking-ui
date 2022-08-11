c518a6a865208dac7ea85e51f3f5cb3522a119f9
Joaco Iannuzzi <joaco.iannuzzi@gmail.com>
2022-08-11 13:17:59 -0300

    all

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,5 +1,7 @@
 import { LoginAsStakerMutation } from "src/generated/graphql";
 import { DeepExtractTypeSkipArrays } from "src/utils/type-utils";
+import type { GetActiveApplicationsForStakersQuery } from "src/generated/graphql";
+
 export enum ApplicationStatusEnum {
   Canceled = "canceled",
   Hired = "hired",
@@ -38,57 +40,57 @@ export enum JobTypeEnum {
   PartTime = "part_time",
 }
 
-export type GetActiveApplicationsForStakersQuery = {
-  __typename?: "Query";
-  getActiveApplicationsForStakers?: Array<{
-    __typename?: "JobApplicationForStaker";
-    id: string;
-    date: string;
-    status: ApplicationStatusEnum;
-    fosterScore: number;
-    candidate: {
-      __typename?: "CandidateProfileForStaker";
-      field: Array<string>;
-      location: string;
-      jobTitle: string;
-      companyName?: string | null;
-      experience?: ExperienceEnum | null;
-      techSkills?: Array<string> | null;
-      softSkills?: Array<string> | null;
-      about?: string | null;
-      available?: boolean | null;
-    };
-    jobAd: {
-      __typename?: "JobAd";
-      id: string;
-      title: string;
-      description: string;
-      responsibilities: string;
-      requirements: string;
-      jobSkills?: Array<string> | null;
-      preferred: string;
-      benefits: string;
-      format: JobTypeEnum;
-      date: string;
-      location?: string | null;
-      isRemote: boolean;
-      currency: string;
-      minSalary: number;
-      maxSalary: number;
-      status: JobStatusEnum;
-      field: FieldEnum;
-      experience: ExperienceEnum;
-      company: {
-        __typename?: "Company";
-        id: string;
-        name: string;
-        photoUrl?: string | null;
-        web?: string | null;
-        description?: string | null;
-      };
-    };
-  }> | null;
-};
+// export type GetActiveApplicationsForStakersQuery = {
+//   __typename?: "Query";
+//   getActiveApplicationsForStakers?: Array<{
+//     __typename?: "JobApplicationForStaker";
+//     id: string;
+//     date: string;
+//     status: ApplicationStatusEnum;
+//     fosterScore: number;
+//     candidate: {
+//       __typename?: "CandidateProfileForStaker";
+//       field: Array<string>;
+//       location: string;
+//       jobTitle: string;
+//       companyName?: string | null;
+//       experience?: ExperienceEnum | null;
+//       techSkills?: Array<string> | null;
+//       softSkills?: Array<string> | null;
+//       about?: string | null;
+//       available?: boolean | null;
+//     };
+//     jobAd: {
+//       __typename?: "JobAd";
+//       id: string;
+//       title: string;
+//       description: string;
+//       responsibilities: string;
+//       requirements: string;
+//       jobSkills?: Array<string> | null;
+//       preferred: string;
+//       benefits: string;
+//       format: JobTypeEnum;
+//       date: string;
+//       location?: string | null;
+//       isRemote: boolean;
+//       currency: string;
+//       minSalary: number;
+//       maxSalary: number;
+//       status: JobStatusEnum;
+//       field: FieldEnum;
+//       experience: ExperienceEnum;
+//       company: {
+//         __typename?: "Company";
+//         id: string;
+//         name: string;
+//         photoUrl?: string | null;
+//         web?: string | null;
+//         description?: string | null;
+//       };
+//     };
+//   }> | null;
+// };
 
 export type JobApplicationForStaker = DeepExtractTypeSkipArrays<
   GetActiveApplicationsForStakersQuery,
